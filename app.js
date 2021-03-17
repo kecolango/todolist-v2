6db776066563ec46e5c6c58263007a5099dfd2f4
Kristian Ecolango <36024287+kecolango@users.noreply.github.com>
2021-03-16 20:57:56 -0500

    Add Delete Items from Custom ToDo Lists Functionality and Lodash

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -14,10 +15,10 @@ app.use(express.static("public"));
 /* const items = ["Buy Food", "Cook Food", "Eat Food"];     // Old method for storing list data
 const workItems = []; */
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});      // Create new db inside MongoDB
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});      // Create new db inside MongoDB
 
 /********** DEPRECATION: This was an addition that was made by me in response to a deprication warning from Mongoose regarding '.findById...''s and '.findOneAnd...''s **********/
-mongoose.set('useFindAndModify', false);
+//mongoose.set('useFindAndModify', false);      // Added to the above '.connect()'
 /********** DEPRECATION END **********/
 
 const itemsSchema = new mongoose.Schema({     // Create schema
@@ -71,7 +72,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/:customListName", function(req, res) {      // 346. Added to allow for dynamic list creation/accessing
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);     // 348. Adds consistency for capitalization of List Titles and url's across browsers and for the titles themselves
   
   List.findOne({name: customListName}, function(err, foundList) {     // '.findOne' returns object, as opposed to '.find()' which is essentially find all, which returns an array
     if(!err) {
@@ -129,15 +130,33 @@ app.post("/", function(req, res){
 
 app.post("/delete", function(req, res) {     // Added to handle checkbox occurence indicating desire to delete an item
   const checkedItemID = req.body.checkbox;
+  const listName = req.body.listName;     // 348. Added to check hidden input added to delete posting form to know which list to delete items from
 
-  Item.findByIdAndRemove(checkedItemID, function(err) {
-    if(err) {
-      console.log(err);
-    }else {
-      console.log("Successfully removed document");
-      res.redirect("/");
-    }
-  });
+  if(listName === "Today") {      // 348. Implemented in order to allow for proper deletion of items on custom lists to occur. Checks for whether the item is being deleted from the default list or a custom one
+    Item.findByIdAndRemove(checkedItemID, function(err) {
+      if(err) {
+        console.log(err);
+      }else {
+        console.log("Successfully removed document");
+        res.redirect("/");
+      }
+    });
+  }else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemID}}}, function(err, foundList) {      // Instructs to look at list with name of 'listName' in order to "pull from the items array an item with an id of 'checkedItemId'". 'foundList' is called such because document that is being found and returned is a list
+      if(!err) {
+        res.redirect("/" + listName);
+      }
+    });
+  }
+  // Item.findByIdAndRemove(checkedItemID, function(err) {
+  //   if(err) {
+  //     console.log(err);
+  //   }else {
+  //     console.log("Successfully removed document");
+  //     res.redirect("/");
+  //   }
+  // });
+  
 });
 
 // app.get("/work", function(req,res){      // Removed and replaced to allow for dynamic lists
